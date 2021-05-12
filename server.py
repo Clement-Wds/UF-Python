0eae03f046161c061991e019e950fa8847ad97b9
Clement-Wds <clementwalshdeserrant@mail.com>
2021-05-12 14:12:15 +0200

    server

--- server.py
+++ server.py
@@ -1,17 +1,59 @@
 import socket
+from _thread import *
+import sys
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.bind(('', 15555))
+
+server = 'localhost'
+port = 5555
+
+serverIp = socket.gethostbyname(server)
+
+try:
+        socket.bind((server, port))
+
+except socket.error as error:
+        print(str(error))
+
+socket.listen(2)
+print('En attente de connexion...')
+
+currentId = "0"
+position = ["0:50,50", "1:100,100"]
+
+def threaded_client(connection):
+        global currentId, position
+        connection.send(str.encode(currentId))
+        currentId = "1"
+        reply = ''
+        while True:
+                try:
+                        data = connection.recv(2048)
+                        reply = data.decode('utf-8')
+                        if not data:
+                                connection.send(str.encode("Au Revoir"))
+                                break
+                        else:
+                                print("Reçu : " + reply)
+                                array = reply.split(":")
+                                id = int(array[0])
+                                position[id] = reply
+
+                                if id == 0:
+                                        nid = 1
+                                if id == 1:
+                                        nid = 0
+                                reply = position[nid][:]
+                                print("Envoi : " + reply)
+
+                        connection.sendall(str.encode(reply))
+                except:
+                        break
+        print("Connexion terminé")
+        connection.close()
 
 while True:
-        socket.listen(5)
-        client, address = socket.accept()
-        print("{} connected".format(address))
+        connection, address = socket.accept()
+        print("Connecté à : ", address)
 
-        response = client.recv(255)
-        if response != "":
-                print(response)
-
-print("Close")
-client.close()
-
+        start_new_thread(threaded_client, (connection,))
