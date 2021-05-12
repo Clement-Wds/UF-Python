af5541af4a2351468a3a72bc19a3a8ae849a5ad6
Clement-Wds <clementwalshdeserrant@mail.com>
2021-05-12 14:15:38 +0200

    fix server_ip

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server = 'localhost'
 port = 5555
 
-serverIp = socket.gethostbyname(server)
+server_ip = socket.gethostbyname(server)
 
 try:
         socket.bind((server, port))
